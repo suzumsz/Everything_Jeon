b8b06c1ffecd007b24621ddf82de83bd486e6c8b
aroob <dlqhfcom@naver.com>
2020-11-11 14:07:26 +0900

    보라 탭추가

--- lib/ReservationPage.dart
+++ lib/ReservationPage.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ReservationApp());
+  runApp(ReservationPage());
 }
 
-class ReservationApp extends StatelessWidget {
+class ReservationPage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
