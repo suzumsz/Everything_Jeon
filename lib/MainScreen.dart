80cd9285a15a0154b1dd2d682717b5ff48b32eca
suzumsz <tnwjd6496@naver.com>
2020-11-26 00:59:26 +0900

    로그인 후 학생증 화면으로 이동

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -15,7 +15,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 2;
 
   List<Widget> _widgetOptions = <Widget>[
     ReservationPage(),
