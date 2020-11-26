7ef97bfc51153a06f40f1e38bf9ad17cd0b765d9
hhyewon <hiimhyewon@naver.com>
2020-11-26 21:06:52 +0900

    위치 Debug 삭제

--- lib/LocationPage.dart
+++ lib/LocationPage.dart
@@ -6,7 +6,7 @@ class LocationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sample App',
+      debugShowCheckedModeBanner: false,
       home: LocationAppPage(),
     );
   }
